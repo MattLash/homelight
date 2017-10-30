a401eaccf0bca9847c0a13fd0026956c12542831
Matt <malashinsky@gmail.com>
2017-10-30 01:03:46 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# README
+
+Setup:
+
+git pull
+
+cd find_an_agent/ruby
+
+bundle
+
+rake db:create
+
+rake db:migrate
+
+rake db:seed
+
+rails s
+
+http://localhost:3000
+
+
+Full Stack Applicants:
+
+  1.) Fill in the AgentSearch#find_agent_matches! method so it returns a better result set
+  2.) Turn the price range radio buttons into checkboxes and use them in the AgentSearch#find_agent_matches! method
+  3.) 
+
+Frontend Candidates:
+
+  1.) Turn the price range radio buttons into checkboxes and make sure the search still works
+  2.) Turn the 3 questions into a "wizard" where you can go forward/back through the question without reloading the page.
+  3.) Add a 100px by 100px circle profile picture to the results page for each agent with an avatar from https://randomuser.me (https://randomuser.me/documentation#howto)
+
+Backend Candidates:
+
+Growth Applicants:
+
+  1.) Add a 100px by 100px circle profile picture to the results page for each agent with an avatar from https://randomuser.me (https://randomuser.me/documentation#howto)
+
+Data Applicants:
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,39 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
+
+
+var var1 = [];
+
+$.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+    
+    // var1 = JSON.parse(data);
+    var1 = data;
+    
+   
+    console.log("this is var1: "+var1);
+    // var1["results"][0].gender
+    console.log("results: " + var1.results[0].gender);
+     return var1;
+    
+  }
+  
+  
+});
